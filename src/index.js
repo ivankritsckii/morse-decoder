71c0eb10ee35e15b7e335df73b91fd549ad78889
Ivan Kritsckii <ivan.kritsckii@yandex.ru>
2023-08-07 08:41:38 +0500

    finish tast

--- src/index.js
+++ src/index.js
@@ -38,6 +38,46 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
+    let result=''
+    let CheckZero=(string)=>{
+        for(let i=0; i<1;i){
+            if(string[i]=='0'){
+                string=string.slice(i+1,string.length-i)
+            }
+            else{
+                i=2
+            }
+        }
+        return string
+    }
+    let decoderInDots=(code)=>{
+        let res=''
+        for(let i=0;i<code.length;i=i+2){
+            if(code[i]+code[i+1]=='10'){
+                res=res+'.'
+            }
+            if(code[i]+code[i+1]=='11'){
+                res=res+'-'
+            }
+        }
+        return res
+    }
+    let decoderInText=(dotsAndDash)=>{
+        if(dotsAndDash==''){
+            result=result+' '
+        }
+        else{
+            result=result+MORSE_TABLE[dotsAndDash]
+        }
+        return result
+    }
+    for (let i=0; i<expr.length; i=i+10){
+        let a=expr.slice(i,i+10);
+        a=CheckZero(a)
+        a=decoderInDots(a)
+        decoderInText(a)
+    }
+    return result
     // write your solution here
 }
 
